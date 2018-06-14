b18d196c20a783f068c0629f429d41cef6f98e14
Alexis <alexistran@berkeley.edu>
2018-06-14 10:12:34 -0700

    added more to changing text

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var text = ["I've also graduated from the #1 public university in the world.", "I've also paddleboarded with sharks.", "I've also pitched a startup idea to VC's in SF.", "I've also DJ-ed for a Fortune 500 company."];
+var text = ["I've also graduated from the #1 public university in the world.", "I've also paddleboarded with sharks.", "I've also pitched a startup idea to VC's in SF.", "I've also DJ-ed for a Fortune 500 company.", "I've also attained Yelp Elite status.", "I've also accidentally applied to art school when I was 8 years old."];
 var counter = 1;
 var elem = document.getElementById("changeText");
 var inst = setInterval(change, 3000);
