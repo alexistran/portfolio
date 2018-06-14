047af97e1fc316c5ded5d9d5d8451e46c8d0540c
Alexis <alexistran@berkeley.edu>
2018-06-14 03:02:25 -0700

    upload files

---
+++ main.js
@@ -0,0 +1,14 @@
+var text = ["I've also graduated from the #1 public university.", "I've also paddleboarded with sharks.", "I've also pitched a startup idea to VC's in SF", "I've also DJ-ed for a Fortune 500 company"];
+var counter = 1;
+var elem = document.getElementById("changeText");
+var inst = setInterval(change, 3000);
+elem.innerHTML = text[0];
+
+function change() {
+  elem.innerHTML = text[counter];
+  counter++;
+  if (counter >= text.length) {
+    counter = 0;
+    // clearInterval(inst); // uncomment this if you want to stop refreshing after one cycle
+  }
+}
